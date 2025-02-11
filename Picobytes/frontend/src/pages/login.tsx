9ad05092c1e54490c8f87f1c235693aaaa83a216
JackMorin25 <114960656+JackMorin25@users.noreply.github.com>
2025-02-11 14:08:39 -0600

    us0.6

--- Picobytes/frontend/src/pages/login.tsx
+++ Picobytes/frontend/src/pages/login.tsx
@@ -1,1 +1,25 @@
-// log in page (possibly with OneLogin)
+//import React from 'react';
+
+
+const Login = () => {
+    
+    return(
+        <div style={{ textAlign: 'center', padding: '50px', backgroundColor: 'gray' }}>
+            <h1 style={{marginBottom: '20px'}}>LOGIN</h1>
+            <div>
+                <label>Username:</label>
+                <input id='UsernameBox' style={{backgroundColor: "white", color: "black", margin: '1em'}}></input>
+            </div>
+            <div>
+                <label>Password:</label>
+                <input id='PasswordBox' style={{backgroundColor: "white", color: "black", margin: '1em'}}></input>
+            </div>
+            <div style={{justifyContent: 'space-between'}}>
+                <button>LOGIN</button>
+                <button>CREATE ACCOUNT</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login

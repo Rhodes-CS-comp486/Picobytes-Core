d56b36242da73310a9344e009152d61f94f5110c
Ryan Kennelly <43864586+0r11@users.noreply.github.com>
2025-02-10 20:54:07 -0600

    created skeleton for question page

--- Picobytes/frontend/src/App.tsx
+++ Picobytes/frontend/src/App.tsx
@@ -1,27 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import './App.css'
-import Homepage from './pages/home'
-import Login from './pages/login'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import "./App.css";
+import Homepage from "./pages/home";
+import Login from "./pages/login";
+import Question from "./pages/question"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-      <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-        
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/question" element={<Question />} />
+          </Routes>
+        </Router>
       </div>
-      <h1>Vite + React</h1>
+
+      {/*sample website code*/}
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -32,9 +34,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Picobytes/frontend/src/pages/question.tsx
+++ Picobytes/frontend/src/pages/question.tsx
@@ -1,1 +1,22 @@
 // The individual question and possibly feedback
+
+import React from "react";
+
+const Question = () => {
+  return (
+    <div>
+      <h1>Example Question</h1>
+      <br></br>
+      <select>
+        <option key="1">Example answer 1</option>
+        <option key="2">Example answer 2</option>
+        <option key="3">Example answer 3</option>
+      </select>
+      <br></br>
+      <br></br>
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default Question;

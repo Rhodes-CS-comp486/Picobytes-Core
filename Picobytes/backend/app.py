b8ad98a585c615b291d11879f2b4a33c79872c9e
Eli Hebert <34279515+elihebert@users.noreply.github.com>
2025-02-11 19:08:35 -0600

    app.py changes

--- Picobytes/backend/app.py
+++ Picobytes/backend/app.py
@@ -1,10 +1,18 @@
 # Treat this as app.py
 from flask import Flask, render_template
+from services.tf_question_pull import pull_questions
+
 app = Flask(__name__, template_folder='../frontend', static_folder='../../public')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/api/questions', methods=['GET'])
+def api_get_questions():
+    questions = pull_questions()
+    return jsonify(questions)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

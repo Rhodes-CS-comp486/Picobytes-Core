f99a64a93f4fd1ccbb2fbe4304ccbddda5531c0a
Ryan Kennelly <43864586+0r11@users.noreply.github.com>
2025-02-13 14:36:41 -0600

    fixed question merge conflict

--- Picobytes/frontend/src/App.tsx
+++ Picobytes/frontend/src/App.tsx
@@ -2,23 +2,11 @@
 import { useState } from "react";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import "./App.css";
+// import "./App.css";
 import Homepage from "./pages/home";
 import Login from "./pages/login";
 import Question from "./pages/question";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-function App() {
-  // const [count, setCount] = useState(0);
-
-//import { useState } from 'react';
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import './App.css';
-import Homepage from './pages/home';
-import Login from './pages/login';
-import Questions from './pages/question';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
@@ -32,7 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/questions" element={<Questions />} />
+            <Route path="/question/:id" element={<Question />} />
           </Routes>
         </Router>
       </div>

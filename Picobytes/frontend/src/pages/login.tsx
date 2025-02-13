b38dba7d510f85fbd366969f83e24929cf12f864
JackMorin25 <114960656+JackMorin25@users.noreply.github.com>
2025-02-13 14:18:11 -0600

    added login html and moved css into its own file

--- Picobytes/frontend/src/pages/login.tsx
+++ Picobytes/frontend/src/pages/login.tsx
@@ -1,28 +1,42 @@
- //import React from 'react';
+ import React from 'react';
+ import { useState } from 'react';
+ import './login.css'
 
 
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = (username: string, password: string) => {
+        //will be changed to store to data base
+        alert(username)
+        alert(password)
+    }
     
+
     return(
-        <div style={{ textAlign: 'center', padding: '50px', backgroundColor: 'gray' }}>
+        <div id='Login'>
             <h1 style={{marginBottom: '20px'}}>LOGIN</h1>
             <div>
                 <label>Username:</label>
-                <input id='UsernameBox' style={{backgroundColor: "white", color: "black", margin: '1em'}}></input>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} id='UsernameBox'></input>
             </div>
             <div>
-                <label>Password:</label>
-                <input id='PasswordBox' style={{backgroundColor: "white", color: "black", margin: '1em'}}></input>
+                <label >Password:</label>
+                <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} id='PasswordBox'></input>
             </div>
-            <div style={{justifyContent: 'space-between'}}>
-                <button>LOGIN</button>
-                <button>CREATE ACCOUNT</button>
+            <div id='ButtonSections'>
+                <div>
+                    <button onClick={() => login(username,password)} id='btnLogin'>LOGIN</button>
+                </div>
+                <div>
+                    <button id='btnCreateAcc'>CREATE ACCOUNT</button>
+                </div>
+                
             </div>
         </div>
     )
 }
 
 export default Login
-// log in page (possibly with OneLogin)
-
-main

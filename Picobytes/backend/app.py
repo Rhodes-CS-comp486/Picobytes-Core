b321512873d90492e9145b853a5155362d782b63
Eli Hebert <34279515+elihebert@users.noreply.github.com>
2025-02-13 14:00:46 -0600

    changes to routing perhaps

--- Picobytes/backend/app.py
+++ Picobytes/backend/app.py
@@ -1,15 +1,13 @@
-# Treat this as app.py
 import os
 from flask import Flask, render_template, jsonify
-from services.tf_question_pull import QuestionService # type: ignore
-import os 
+from services.tf_question_pull import QuestionService
 
 # get absolute path of current file's directory
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 # configure paths relative to the base directory
 frontend_dir = os.path.join(base_dir, 'frontend')
-public_dir = os.path.join(base_dir, 'public')
+public_dir = os.path.join(frontend_dir, 'public')
 
 app = Flask(__name__, 
             template_folder=frontend_dir, 
@@ -25,7 +23,6 @@ def home():
 def api_get_questions():
     questions = question_service.pull_questions()
     return jsonify(questions)
-
 
 if __name__ == '__main__':
     app.run(debug=True)

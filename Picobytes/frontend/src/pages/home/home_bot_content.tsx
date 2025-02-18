ebf5fd9beede978a18e1edab0d4d4c2a872ef285
KellyVKT <157765636+KellyVKT@users.noreply.github.com>
2025-02-18 15:13:07 -0600

    Add total question on homepage

---
+++ Picobytes/frontend/src/pages/home/home_bot_content.tsx
@@ -0,0 +1,24 @@
+/* Homepage Bot Contents */
+
+//import { useNavigate } from 'react-router-dom'
+import './home_bot_content.css'
+
+const Home_Bot_Content = () => {
+    return (
+        
+        <div className='hbc'>
+            <h1>Lessons</h1>
+            <div className='hbc-grid'>
+                <div>
+                    <button>click me</button>
+                </div>
+                <div>
+                    <button>click me</button>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home_Bot_Content

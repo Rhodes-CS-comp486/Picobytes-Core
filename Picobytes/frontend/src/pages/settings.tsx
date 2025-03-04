5fd240f807395391f499ec3d39b9f8db278330c1
Ryan Kennelly <43864586+0r11@users.noreply.github.com>
2025-03-04 14:55:56 -0600

    added adjustable font size

--- Picobytes/frontend/src/pages/settings.tsx
+++ Picobytes/frontend/src/pages/settings.tsx
@@ -3,9 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 interface Prop {
   toggleDark: () => void;
+  fontSize: number;
+  setFontSize: (size: number) => void;
 }
 
-const Settings = ({ toggleDark }: Prop) => {
+const Settings = ({ toggleDark, fontSize, setFontSize }: Prop) => {
   const navigate = useNavigate();
   //   const { toggleDarkMode } = useContext(ThemeContext);
 
@@ -17,6 +19,20 @@ const Settings = ({ toggleDark }: Prop) => {
       <button className="btn-toggle" onClick={() => toggleDark()}>
         ☾
       </button>
+
+      <p>Set Font Size</p>
+      <div className="slidecontainer">
+        <input
+          type="range"
+          min="10"
+          max="50"
+          value={fontSize}
+          className="slider"
+          id="myRange"
+          onChange={(e) => setFontSize(e.target.valueAsNumber)}
+        />
+        <p>{fontSize}</p>
+      </div>
       <br></br>
       <br></br>
       <button onClick={() => navigate("/homepage")}>Home</button>

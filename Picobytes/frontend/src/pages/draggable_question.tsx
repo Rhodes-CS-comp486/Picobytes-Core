208819ad4c9649f7bb587bc265fc0f3723b96a7a
JackMorin25 <114960656+JackMorin25@users.noreply.github.com>
2025-03-06 12:23:28 -0600

    us.10 backend connection changes still not working

--- Picobytes/frontend/src/pages/draggable_question.tsx
+++ Picobytes/frontend/src/pages/draggable_question.tsx
@@ -2,8 +2,28 @@ import React, { useState } from "react";
 import './draggable_question.css'
 
 const Draggable_Question = () => {
+  const [answer, setAnswer] = useState<boolean[] | boolean | null>([false, false, false, false]);
+  const [questionType, setQuestionType] = useState<string>("multiple_choice");
+  const [correct, setCorrect] = useState<number | boolean>(0);
+  const [options, setOptions] = useState([
+    "Option 1",
+    "Option 2",
+    "Option 3",
+    "Option 4",
+    "Option 5",
+    "Option 6",
+    "Option 7",
+    "Option 8",
+    "Option 9",
+    "Option 10",
+  ]);
+  const [error, setError] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [topic, setTopic] = useState("");
+  const [totalQuestions, setTotalQuestions] = useState(0);
   const [draggedItem, setDraggedItem] = useState<HTMLElement | null>(null);
-  const questions = ["Question 1", "A 2", "Question 3", "Question 4", "Question 5"];
+  const [questionText, setQuestionText] = useState("");
+  const [questions, setQuestion] = useState(["Question 1", "A 2", "Question 3", "Question 4", "Question 5"]);
   
 
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>, item: HTMLElement) => {
@@ -24,26 +44,33 @@ const Draggable_Question = () => {
 
   const questionPull = async (qid: number) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/questions/${qid}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
+      const response = await fetch(`http://127.0.0.1:5000/api/question/${qid}`);
       const data = await response.json();
-
-      console.log(data);
-
-      if (!response.ok) {
-        throw new Error(data.error || "Failed to get questions");
+  
+      if (data.error) throw new Error(data.error);
+  
+      console.log("Question data:", data);
+      setQuestionText(data.question_text);
+      setQuestionType(data.question_type);
+      setDifficulty(data.question_level);
+      setTopic(data.question_topic);
+  
+      if (data.question_type === "code_blocks") {
+        setQuestion(
+          [data.block1, data.block2, data.block3,
+           data.block4, data.block5, data.block6,
+           data.block7, data.block8, data.block9, data.block10].filter(Boolean)
+        );
+  
+        setCorrect(data.answer ?? "");
       }
-
-      
     } catch (err: any) {
-      console.error(err.message);
+      console.error("Error fetching question:", err.message);
+      setError(err.message);
     }
-  }
+  };
+  
+  
 
     return (
         <div className='container'>

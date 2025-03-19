59c2380c1d707981c9ce8c0868735535ea84e2a0
Ryan Kennelly <43864586+0r11@users.noreply.github.com>
2025-03-19 17:32:37 -0500

    created basic functions for sending emails (not currently hooked up to db or scheduled)

---
+++ Picobytes/backend/email/reminder_email.py
@@ -0,0 +1,15 @@
+from send_email import send
+import datetime
+
+def reminder(name : str, n : int, mail : str): 
+    d = datetime.datetime.now()
+
+    formatted_date = d.strftime("(%B %d, %Y)")
+
+    subject = f"Come back to Pico Bytes {formatted_date}"
+
+    message = f"{name}, \n\nYou haven't been on Pico Bytes for {n} days.\nCome back!\n\nhttp://localhost:5173"
+
+    unsubscribe = f"\n\nunsubscribe: http://localhost:5173/unsubscribe/{mail} "
+
+    send(subject, message + unsubscribe, [mail])

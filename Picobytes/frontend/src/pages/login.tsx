4d786c1e9bf1c55119a4e7578f80b0fdfef82dfc
Eli Hebert <34279515+elihebert@users.noreply.github.com>
2025-03-25 13:31:48 -0500

    login modifications

--- Picobytes/frontend/src/pages/login.tsx
+++ Picobytes/frontend/src/pages/login.tsx
@@ -22,7 +22,7 @@ const Login = () => {
       setLoading(true);
       setError(null);
 
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("http://127.0.0.1:5000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
